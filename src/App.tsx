be4a642a324422149e608a6ba368e524078538d5
augustinemartins <augustinemartins4real@gmail.com>
2026-06-23 13:11:48 +0000

    addsnapshot

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import { Dashboard } from './pages/Dashboard'
@@ -20,22 +20,29 @@ function PriceDetailLoader() {
 
 const BASENAME = import.meta.env.BASE_URL.replace(/\/$/, '')
 
+function AppContent() {
+  const location = useLocation()
+  return (
+    <ErrorBoundary key={location.key}>
+      <PreferencesProvider>
+        <Layout>
+          <Suspense fallback={<PriceDetailLoader />}>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/price/:pair" element={<PriceDetail />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </Layout>
+      </PreferencesProvider>
+    </ErrorBoundary>
+  )
+}
+
 export default function App() {
   return (
     <BrowserRouter basename={BASENAME}>
-      <ErrorBoundary>
-        <PreferencesProvider>
-          <Layout>
-            <Suspense fallback={<PriceDetailLoader />}>
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/price/:pair" element={<PriceDetail />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </Suspense>
-          </Layout>
-        </PreferencesProvider>
-      </ErrorBoundary>
+      <AppContent />
     </BrowserRouter>
   )
 }
